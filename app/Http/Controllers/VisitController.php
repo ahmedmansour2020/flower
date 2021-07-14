3cbfbfc3acbd326f41055382d36d38ba657e6a51
ahmedmansour2020 <al3etreby_92@yahoo.com>
2021-07-14 14:44:07 +0200

    count visits

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -9,27 +9,27 @@ use Illuminate\Support\Facades\Auth;
 class VisitController extends Controller
 {
     public static function record_visit(){
-        // $visits=Setting::where('key','visits')->first();
-        // if(!$visits){
-        //     $visits=new Setting();
-        //     $visits->type="session";
-        //     $visits->key="visits";
-        //     $visits->value=0;
-        //     $visits->save();
-        // }
-        // if(!request()->session()->exists('flowers')){
-        //     session(['flowers' => 'visit']);
-        //     $visits->value+=1;
-        //     $visits->save();
-        // }
+        $visits=Setting::where('key','visits')->first();
+        if(!$visits){
+            $visits=new Setting();
+            $visits->type="session";
+            $visits->key="visits";
+            $visits->value=0;
+            $visits->save();
+        }
+        if(!isset($_COOKIE['visitor'])) {
+            setcookie('visitor', 'flowers', time() + (86400 * 365*10), "/");
+            $visits->value+=1;
+            $visits->save();
+          } 
     }
 
     public static function count_visits(){
-        // $visits=Setting::where('key','visits')->first();
-        // if($visits){
-        //     return $visits->value;
-        // }else{
-        //     return 0;
-        // }
+        $visits=Setting::where('key','visits')->first();
+        if($visits){
+            return $visits->value;
+        }else{
+            return 0;
+        }
     }
 }
