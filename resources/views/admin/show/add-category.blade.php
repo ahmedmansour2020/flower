e4d393e9909a3280bf3e6c97879462c2d790fa93
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2021-07-14 15:58:10 +0200

    edit style

--- resources/views/admin/show/add-category.blade.php
+++ resources/views/admin/show/add-category.blade.php
@@ -1,32 +1,34 @@
 @extends('dashboard-layouts.layout-admin')
 @section('title', isset($title) ? $title : '')
 @section('page_css')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap.min.css" />
 @endsection
 @section('content')
 
- 
-<div class="container">
-<div class="row">
-@if($action=='add')
-<form method="post" action="{{route('category.store')}}" enctype="multipart/form-data">
-@else
-<form method="post" action="{{route('category.update',$category->id)}}" enctype="multipart/form-data">
-@method('PUT')
-@endif
-{{ csrf_field() }} 
-<div class="form-group col-12">
-<input type="text" name="name" class="form-control" placeholder="اسم التصنيف" value="{{$action=='update'?$category->name:""}}" />
-</div>
 
-<button type="submit">حفظ</button>
+    <div class="container">
+        <div class="row">
+            @if ($action == 'add')
+                <form method="post" action="{{ route('category.store') }}" enctype="multipart/form-data">
+                @else
+                    <form method="post" action="{{ route('category.update', $category->id) }}" enctype="multipart/form-data">
+                        @method('PUT')
+            @endif
+            {{ csrf_field() }}
+            <div class="form-group col-12">
 
-</form>
-</div>
- </div>
+                <input type="text" name="name" class="inp-category" placeholder="اسم التصنيف"
+                    value="{{ $action == 'update' ? $category->name : '' }}" />
+            </div>
+
+            <button type="submit" class="btn fixed-style-btn m-auto">حفظ</button>
+
+            </form>
+        </div>
+    </div>
 
 
 @endsection
 @section('page_js')
-<script src="{{asset('resources/assets/js/shops.js')}}"></script>
+    <script src="{{ asset('resources/assets/js/shops.js') }}"></script>
 @endsection
